ec19a39c89b1ea7f4bc08db1c8199559ead37961
Wachira Mureithi <wachiramureithi@live.com>
2022-09-09 09:21:34 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,26 @@
+#include <stdio.h>                                                                                                              
+
+                                                                                                                                
+
+# main - print the string in the put function                                                                                   
+
+                                                                                                                                
+
+# description usingthe main function                                                                                            
+
+                                                                                                                                
+
+# Programming is like building a multilingual puzzle                                                                            
+
+                                                                                                                                
+
+# return 0                                                                                                                      
+
+                                                                                                                                
+
+int main (void)                                                                                                                 
+
+{
+   puts("\"Programming is like building a multilingual puzzle");
+   return (0);
+}

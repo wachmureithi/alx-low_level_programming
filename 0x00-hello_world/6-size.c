2cc2a425535aa069959997943350dfa73d750e3d
Wachira Mureithi <wachiramureithi@live.com>
2022-09-12 07:56:31 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,43 +1,26 @@
-#include <stdio.h>                                                                                                            
+#include <stdio.h>
 
-                                                                                                                              
+/**
+* main - print the string in the printf function
+* 
+* Description: using the main function
+* this program prints the size of types of variable
+* Return: 0
+*/
 
-/**                                                                                                                           
+int main(void)
+{
+char c;
+int i;
+long li;
+long long lli;
+float f;
 
-*main - print the string in the printf function                                                                               
-
-*                                                                                                                             
-
-*Description: using the main function                                                                                         
-
-*this program prints the size of types of variable                                                                            
-
-*Return: 0                                                                                                                    
-
-*/                                                                                                                            
-
-                                                                                                                              
-
-int main(void)                                                                                                                
-
-{                                                                                                                             
-
-char c;                                                                                                                       
-
-int i;                                                                                                                        
-
-long int li;                                                                                                                  
-
-long long lli;                                                                                                                
-
-float f;                                                                                                                      
-
-                                                                                                                              
-
-printf("Size of a char: %ld byte(s)\n", sizeof(c));
+printf("Size of a char: %ld byte(s)\n", sizeof(c))
 printf("Size of an int: %ld byte(s)\n", sizeof(i));
 printf("Size of a long int: %ld bytes(s)\n", sizeof(li));
 printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
 printf("Size of a float: %ld byte(s)\n", sizeof(f));
 return (0);
 }
+

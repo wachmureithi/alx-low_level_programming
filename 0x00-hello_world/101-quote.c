9b4f070ce9ab4122585ba7424bc5179ee84a6b71
Wachira Mureithi <wachiramureithi@live.com>
2022-09-12 08:25:39 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,11 +6,10 @@
 * - Dora Korpar, 2015-10-19"
 * followed by a new line, to the standard error.
 * Return: Always 0 (Success)
-*/
+*/ 
 
-int main(voi)
+int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
-

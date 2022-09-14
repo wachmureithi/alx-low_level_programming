8362379c81f39d76810e87b5b303e5318c3dd12a
wachmureithi <wachiramureithi@live.com>
2022-09-14 03:15:14 -0700

    prints the alphabet in lowercase, followed by a new line

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-/**
-* main - prints the alphabet in lowercase, followed by a new line
+/** 
+* main - prints the alphabet in lowercase, followed by a new line 
 * Return: Always 0 (Success)
 */
 int main(void)
@@ -16,6 +16,5 @@ int main(void)
 		letter++;
 	}
 	putchar('\n');
-	
 	return (0);
 }

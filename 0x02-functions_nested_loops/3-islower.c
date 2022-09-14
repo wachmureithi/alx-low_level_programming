caae85a9d2b94b49c64e03de1f63a8922e615699
wachmureithi <wachiramureithi@live.com>
2022-09-14 07:53:53 -0700

    prints the alphabets in lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - prints the alphabets in lower case
+ * @alpha: number or an alphabet
+ * Returns 1 if c is lowercase
+ * Returns 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if (c  >= 'a' && alpha  <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

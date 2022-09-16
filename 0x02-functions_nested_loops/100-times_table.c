2bc7cd419e9e3b03d8a091f6bb9e13b1fd8db4e5
wachmureithi <wachiramureithi@live.com>
2022-09-15 19:27:34 -0700

    101-natural

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,11 +1,9 @@
 #include "main.h"
 
-/**
- *
+/** 
  * print_times_table - prints the n times table, starting with 0
- *  @n: number of the times table
+ * @n: number of the times table
  */
-
 void print_times_table(int n)
 {
 	int i, j, k;
@@ -13,6 +11,7 @@ void print_times_table(int n)
 	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
+		
 		{
 			for (j = 0; j <= n; j++)
 			{
@@ -21,8 +20,10 @@ void print_times_table(int n)
 				{
 					_putchar(k + '0');
 				}
-				else if (k < 10 && j != 0
-				{	_putchar(',');
+				 else if (k < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
@@ -48,4 +49,5 @@ void print_times_table(int n)
 			_putchar('\n');
 		}
 	}
+	
 }

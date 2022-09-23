a86b28945b67c38d81fd45270b190241acf80f28
wachmureithi <wachiramureithi@live.com>
2022-09-23 09:39:58 -0700

    Prints an integer.

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_number - Prints an integer
+ * print_number - Prints an integer.
  * @n: The integer to be printed.
  */
 
@@ -17,6 +17,6 @@ void print_number(int n)
 
 	if ((num / 10) > 0)
 		print_number(num / 10);
-	
+
 	_putchar((num % 10) + '0');
 }
